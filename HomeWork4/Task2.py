a0c69af8f58efb1e8770d11b4f228cdeaa728bbd
Elenaorus <110274316+Elenaorus@users.noreply.github.com>
2023-08-07 21:41:16 +0700

    added a HomeWork4

---
+++ HomeWork4/Task2.py
@@ -0,0 +1,12 @@
+def kwargs_to_dict(**kwargs):
+    result = {}
+    for key, value in kwargs.items():
+        try:
+            result[value] = key
+        except:
+            result[str(value)] = key
+    return result
+
+print(kwargs_to_dict(name='Алексей', sername='Нестеров', weight=35.5,
+                     months=['January', 'February', 'March'],
+                     courses={'python': 'ver 3.11', 'c#': 'ver 2.5'}))
